a6c5ed9becae3ff08877b4dfae91dc2bcbf3c38b
Tin Nguyen <TNguyen@Tins-Mac-mini.local>
2014-03-13 18:37:55 -0500

    added a test class

---
+++ PVTestGit.h
@@ -0,0 +1,13 @@
+//
+//  PVTestGit.h
+//  GitTest
+//
+//  Created by Tin Nguyen on 3/13/14.
+//  Copyright (c) 2014 Tin Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PVTestGit : NSObject
+
+@end
---
+++ PVTestGit.m
@@ -0,0 +1,13 @@
+//
+//  PVTestGit.m
+//  GitTest
+//
+//  Created by Tin Nguyen on 3/13/14.
+//  Copyright (c) 2014 Tin Nguyen. All rights reserved.
+//
+
+#import "PVTestGit.h"
+
+@implementation PVTestGit
+
+@end
